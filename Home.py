8b851c0bab959a88dd6ce6232a6bf7f9d2fa8d5f
FESL Sapphire Dev <codspecialops@gmail.com>
2023-02-01 14:40:52 -0600

    Create columns, image, and description

--- Home.py
+++ Home.py
@@ -1,3 +1,35 @@
 import streamlit as sl
+import pandas as pd
 
-sl.title("Home")
+sl.set_page_config(layout="wide")
+
+col1, col2 = sl.columns(2)
+
+with col1:
+	sl.image("images/image.png")
+
+with col2:
+	sl.subheader("Goutham Pedinedi")
+	content = """
+Hello there, fellow citizens. I am Goutham Pedinedi, a programmer with a year of experience 
+in programming languages such as C++, Python, and Ruby. I live in the United States of America,
+although I come from India. I haven't worked at any companies yet, but I hope to do so to gain
+experience and set up my own business or work in Google or Microsoft (definitely not Apple because
+I hate it).
+	"""
+
+	sl.info(content)
+
+sl.write("Below you can find some of the apps I have built in Python:")
+
+col3, col4, = sl.columns(2)
+
+data = pd.read_csv("data.csv", sep=";")
+
+with col3:
+	for index, row in data[:11].iterrows():
+		sl.header(row["title"])
+
+with col4:
+	for index, row in data[11:].iterrows():
+		sl.header(row["title"])
--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,1 +1,6 @@
 import streamlit as sl
+
+sl.write("Your email address")
+sl.text_input("", placeholder="Enter email address. Ex: youwho@gnpmail.com")
+sl.write("Your message")
+sl.text_input("", placeholder="Enter your feedback")
